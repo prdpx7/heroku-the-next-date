c3225c8957868cdffe78efe9fdeabff89eae0150
zuck007 <pradeepchoudhary009@gmail.com>
2018-02-15 05:27:29 +0530

    cors fixed

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const tweep = require('tweep')
 const  app = express()
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 app.get('/', (req, res)=> res.send('Hello world!'))
 app.get('/twitter/:username/:count?', (req, res)=>{
     // console.log(req)
